c96bd17eeb32f7ea90b33159ad643ae1dae44456
Vikas <vikasreddy2313@gmail.com>
2022-07-31 18:22:51 +0900

    update

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-let currentMusic = 0;
 const music = document.querySelector("#audio");
 const numOfSongs = document.querySelector(".music-counter");
 const seekBar = document.querySelector(".seek-bar");
@@ -8,17 +7,143 @@ const currentTime = document.querySelector(".current-time");
 const musicDuration = document.querySelector(".song-duration");
 const shuffleBtn = document.querySelector(".random-track");
 const prevbtn = document.querySelector(".prev-track");
-const playBtn = document.querySelector(".pause-track");
+const playBtn = document.querySelector(".play-track");
 const nextBtn = document.querySelector(".next-track");
 const rewindBtn = document.querySelector(".repeat-track");
 const libraryBtn = document.querySelector(".btn--library");
 const lyricsBtn = document.querySelector(".btn--lyrics");
 const sideBar = document.querySelector(".sidebar");
+const disk = document.querySelector(".track-image");
+const songList = document.querySelector(".song");
+let counter = songs.length - 1;
+let currentMusic = 0;
+const displayLibrary = function (arr) {
+  songList.innerHTML = "";
 
-changePauseBtn = (icon) => icon.classList.toggle("fa-pause");
-openLibrary = (sidebar) => sidebar.classList.toggle("hidden");
+  arr.forEach(function () {
+    console.log(counter);
+    const html = `
+      <div class="img-container">
+      <div class="img">
+       <img class="img-cover" src="${arr[counter].cover}" alt="${arr[counter].name}">
+      </div>
+      </div>
+        <div class="song-info">
+        <h3 class="composer">${arr[counter].name}</h3>
+        <h3 class="band-name">${arr[counter].artist}</h3>
+      </div>
+    `;
+    counter--;
+    songList.insertAdjacentHTML("afterbegin", html);
+  });
+};
 
-// libraryBtn.addEventListener("click", function (e) {
-//   e.preventDefault();
-//   sideBar.classList.remove(".hidden");
-// });
+//Oraganize Library
+libraryBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  sideBar.classList.toggle("active");
+});
+
+//Pause/Play Button
+playBtn.addEventListener("click", function () {
+  if (playBtn.className.includes("pause")) {
+    music.play();
+  } else {
+    music.pause();
+  }
+  playBtn.classList.toggle("pause");
+  disk.classList.toggle("play");
+});
+
+//Start Music Player
+const setMusic = (i) => {
+  seekBar.value = 0;
+  let song = songs[i];
+  currentMusic = i;
+  music.src = song.path;
+  music.load();
+
+  songName.innerHTML = song.name;
+  artistName.innerHTML = song.artist;
+  numOfSongs.textContent =
+    "Playing " + (currentMusic + 1) + " of " + songs.length;
+  disk.style.backgroundImage = `url(${songs[currentMusic].cover})`;
+  currentTime.innerHTML = "00:00";
+  setTimeout(() => {
+    seekBar.max = music.duration;
+    musicDuration.innerHTML = formatTime(music.duration);
+  }, 300);
+};
+
+//Format Time
+const formatTime = (time) => {
+  let min = Math.floor(time / 60);
+  if (min < 10) {
+    min = `0${min}`;
+  }
+  let sec = Math.floor(time % 60);
+  if (sec < 10) {
+    sec = `0${sec}`;
+  }
+  return `${min} : ${sec}`;
+};
+
+//Seekbar
+setInterval(() => {
+  seekBar.value = music.currentTime;
+  currentTime.innerHTML = formatTime(music.currentTime);
+  if (Math.floor(music.currentTime) == Math.floor(seekBar.max)) {
+    nextBtn.click();
+  }
+}, 500);
+
+seekBar.addEventListener("change", () => {
+  music.currentTime = seekBar.value;
+});
+
+//Forward Button
+nextBtn.addEventListener("click", () => {
+  if (currentMusic >= songs.length - 1) {
+    currentMusic = 0;
+  } else {
+    currentMusic++;
+  }
+  setMusic(currentMusic);
+  playMusic();
+});
+
+// Backwards Button
+prevbtn.addEventListener("click", () => {
+  if (currentMusic <= 0) {
+    currentMusic = songs.length - 1;
+  } else {
+    currentMusic--;
+  }
+  setMusic(currentMusic);
+  playMusic();
+});
+
+//Rewind Button
+rewindBtn.addEventListener("click", () => {
+  setMusic(currentMusic);
+  playMusic();
+});
+
+//Random Button
+shuffleBtn.addEventListener("click", () => {
+  randomNum = Math.floor(Math.random() * songs.length);
+  if (randomNum === currentMusic) {
+    randomNum = Math.floor(Math.random() * songs.length);
+  }
+  setMusic(randomNum);
+  playMusic();
+});
+
+const playMusic = () => {
+  music.play();
+  playBtn.classList.remove("pause");
+  disk.classList.remove("play");
+};
+
+setMusic(0);
+displayLibrary(songs);
---
+++ data.js
@@ -0,0 +1,20 @@
+const song1 = {
+  name: "逆夢",
+  path: "music/Sakayume.mp3",
+  artist: "King Gnu",
+  cover: "img/king-gnu",
+};
+const song2 = {
+  name: "一途",
+  path: "music/Ichizu.mp3",
+  artist: "King Gnu",
+  cover: "img/king-gnu",
+};
+const song3 = {
+  name: "神っぽいな",
+  path: "music/Kamipoina.mp3",
+  artist: "Ado",
+  cover: "img/kamipoina",
+};
+
+let songs = [song1, song2, song3];
