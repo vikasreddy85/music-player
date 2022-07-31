7536fe1e0eabfdae0b80a720c806c7219378b703
Vikas <vikasreddy2313@gmail.com>
2022-08-01 00:34:53 +0900

    update

--- data.js
+++ data.js
@@ -1,20 +1,404 @@
 const song1 = {
+  name: "セーラ☆ムン太郎",
+  path: "music/maharajan.mov",
+  artist: "マハラージャン",
+  cover:
+    "https://is4-ssl.mzstatic.com/image/thumb/Music126/v4/43/f0/98/43f098da-14e2-be64-36f1-f898a331ede7/4547366538113.jpg/1200x1200bf-60.jpg",
+};
+
+const song2 = {
+  name: "帰ろ",
+  path: "music/kaero.mp3",
+  artist: "Fuji Kaze",
+  cover:
+    "https://www.billboard.com/wp-content/uploads/2021/11/Fujii-Kaze-press-2021-billboard-1548.jpg",
+};
+const song3 = {
+  //one
+  name: "優しさの剣",
+  path: "music/novelone.mp3",
+  artist: "Novelbright",
+  cover:
+    "https://yt3.ggpht.com/7o4Wdc51o2uhiaeq9SDYYVLl7hj96YevA5nEtqXQXD0aftDGRVkx6nQALJyTpX9GuxnQ5n3JF00=s900-c-k-c0x00ffffff-no-rj",
+};
+
+const song4 = {
+  name: "ココロオドル",
+  path: "music/kokoro.mp3",
+  artist: "nobodyknows+",
+  cover: "https://i.ytimg.com/vi/XaVPr6HVrbI/maxresdefault.jpg",
+};
+
+const song5 = {
+  //three
+  name: "ツキミソウ",
+  path: "music/noveltwo.mp3",
+  artist: "Novelbright",
+  cover:
+    "https://yt3.ggpht.com/7o4Wdc51o2uhiaeq9SDYYVLl7hj96YevA5nEtqXQXD0aftDGRVkx6nQALJyTpX9GuxnQ5n3JF00=s900-c-k-c0x00ffffff-no-rj",
+};
+
+const song6 = {
   name: "逆夢",
   path: "music/Sakayume.mp3",
   artist: "King Gnu",
-  cover: "img/king-gnu",
+  cover: "https://images-fe.ssl-images-amazon.com/images/I/41WKgaVegFL.jpg",
 };
-const song2 = {
-  name: "一途",
-  path: "music/Ichizu.mp3",
-  artist: "King Gnu",
-  cover: "img/king-gnu",
+
+const song7 = {
+  name: "Citrus",
+  path: "music/citrus.mp3",
+  artist: "Da-Ice",
+  cover: "https://img.imageimg.net/artist/daice/img/product_1033660.jpg",
 };
-const song3 = {
+
+const song8 = {
+  name: "宿命",
+  path: "music/shukumei.mp3",
+  artist: "Official髭男dism",
+  cover:
+    "https://yt3.ggpht.com/ytc/AMLnZu9EF-SsY_iGEhHmaLcNiyGIwb8JZGDSHnci7pE3Eg=s900-c-k-c0x00ffffff-no-rj",
+};
+const song9 = {
   name: "神っぽいな",
   path: "music/Kamipoina.mp3",
   artist: "Ado",
-  cover: "img/kamipoina",
+  cover:
+    "https://www.lyrical-nonsense.com/wp-content/uploads/2021/09/PinocchioP-God-ish.jpg",
 };
 
-let songs = [song1, song2, song3];
+const song10 = {
+  name: "Pale Blue",
+  path: "music/pale-blue.mp3",
+  artist: "Kenshi Yonezu",
+  cover:
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWR9ZvdV5qd2EroI9P-K7la8QhX6IYvSMhwg&usqp=CAU",
+};
+
+const song11 = {
+  name: "Habit",
+  path: "music/habit.mp3",
+  artist: "Sekai no Owari",
+  cover:
+    "https://i.discogs.com/5mOat0edrY87EGBaocXfKqMYZkTRN0KVBJjbe8wRC6E/rs:fit/g:sm/q:90/h:479/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9BLTQ1MTI0/MzItMTQ4MTE5OTY1/MC0zMDY1LnBuZw.jpeg",
+};
+
+const song12 = {
+  name: "レディメイド",
+  path: "music/ready.mp3",
+  artist: "Ado",
+  cover:
+    "https://yt3.ggpht.com/nt3XgEI74R1VvNmQN22CLrSuEE838KKN5e9XpHpzcRvGIbAOVtTdYr_zR35YHiuYufEKaP3osg=s900-c-k-c0x00ffffff-no-rj",
+};
+
+const song13 = {
+  name: "別の人の彼女",
+  path: "music/kanojo .mp3",
+  artist: "wacci",
+  cover:
+    "https://www.thefirsttimes.jp/admin/wp-content/uploads/2022/06/20220625-st-104901-1408x938.jpg",
+};
+const song14 = {
+  name: "Hello",
+  path: "music/hello.mp3",
+  artist: "Official髭男dism",
+  cover:
+    "https://yt3.ggpht.com/ytc/AMLnZu9EF-SsY_iGEhHmaLcNiyGIwb8JZGDSHnci7pE3Eg=s900-c-k-c0x00ffffff-no-rj",
+};
+
+const song15 = {
+  name: "Inferno",
+  path: "music/inferno.mp3",
+  artist: "Mrs. Green Apple",
+  cover: "https://m.media-amazon.com/images/I/51xBRCUkIRL._SS500_.jpg",
+};
+
+const song16 = {
+  name: "Marunoichi 1",
+  path: "music/guitar.mp3",
+  artist: "Osamuraisan",
+  cover: "https://i1.sndcdn.com/artworks-000142707979-fbh7db-t500x500.jpg",
+};
+
+const song17 = {
+  name: "ヒッチコック",
+  path: "music/hitch.mp3",
+  artist: "ヨルシカ",
+  cover: "https://sp.universal-music.co.jp/yorushika/matasaburou/img/jkt.jpg",
+};
+
+const song18 = {
+  name: "Marunoichi 2",
+  path: "music/marunoichi.mp3",
+  artist: "めいちゃん",
+  cover:
+    "https://yt3.ggpht.com/ytc/AKedOLT7246eM4g2AwoED1cZGlyzy60auMoWkhZPm2WDyg=s900-c-k-c0x00ffffff-no-rj",
+};
+const song19 = {
+  name: "一途",
+  path: "music/Ichizu.mp3",
+  artist: "King Gnu",
+  cover: "https://images-fe.ssl-images-amazon.com/images/I/41WKgaVegFL.jpg",
+};
+
+const song20 = {
+  name: "Lost In Paradice",
+  path: "music/ali.mp3",
+  artist: "Ali",
+  cover: "https://m.media-amazon.com/images/I/61JNvrApHRL._AC_SL1500_.jpg",
+};
+
+const song21 = {
+  name: "ERROR",
+  path: "music/-ERROR_常闇トワ(cover).mp3",
+  artist: "niki",
+  cover: "https://cdn.karent.jp/thumb_a/084/1084_20120711182720_0400_0400.jpg",
+};
+
+const song22 = {
+  name: "馬鹿",
+  path: "music/【オリジナルMV】馬鹿 歌いました【Ado】.mp3",
+  artist: "Ado",
+  cover:
+    "https://yt3.ggpht.com/nt3XgEI74R1VvNmQN22CLrSuEE838KKN5e9XpHpzcRvGIbAOVtTdYr_zR35YHiuYufEKaP3osg=s900-c-k-c0x00ffffff-no-rj",
+};
+
+const song23 = {
+  name: "少女レイ",
+  path: "music/【伊東歌詞太郎】少女レイ【歌ってみた】.mp3",
+  artist: "伊東歌詞太郎",
+  cover:
+    "https://komachi-mag.com/wp-content/uploads/2019/08/d9463ab3838f3b11fef8f0903e497990.jpg",
+};
+
+const song24 = {
+  name: "クリスマスソング",
+  path: "music/back number - 「クリスマスソング」Music Video.mp3",
+  artist: "back number",
+  cover:
+    "https://yt3.ggpht.com/ytc/AMLnZu_MMeTfVMSHDwUOr7YtOCeHZ-latWgc1qMJS0M6=s900-c-k-c0x00ffffff-no-rj",
+};
+
+const song25 = {
+  name: "ray",
+  path: "music/BUMP OF CHICKEN「ray」.mp3",
+  artist: "BUMP OF CHICKEN",
+  cover:
+    "https://yt3.ggpht.com/EtnABrrH7fs0Ze1hyOtxq8aJH6rpXQw0c_mBSaLeTYc2St-QeIjS55rCA0FJkfOlXRQjnlR9=s900-c-k-c0x00ffffff-no-rj",
+};
+
+const song26 = {
+  name: "猫",
+  path: "music/Dish__ - Neko 猫 _ Lyrics Video.mp3",
+  artist: "Dish",
+  cover:
+    "https://www.sonymusic.co.jp/Music/Info/DISH/neko/assets/images/photo-jk.jpg",
+};
+
+const song27 = {
+  name: "COLORS",
+  path: "music/flow.mp3",
+  artist: "FLOW",
+  cover: "https://img.mymusic.net.tw/mms/album/L/922/248922.jpg",
+};
+
+const song28 = {
+  name: "Kirari",
+  path: "music/Fujii Kaze - Kirari (Official Video).mp3",
+  artist: "Fuji Kaze",
+  cover:
+    "https://www.billboard.com/wp-content/uploads/2021/11/Fujii-Kaze-press-2021-billboard-1548.jpg",
+};
+
+const song29 = {
+  name: "ダビングシーン",
+  path: "music/indi.mp3",
+  artist: "indigo la End",
+  cover: "https://img.ananweb.jp/2019/10/12123242/311-455x600.jpg",
+};
+const song30 = {
+  name: "夏夜のマジック",
+  path: "music/indigo la End「夏夜のマジック」.mp3",
+  artist: "indigo la End",
+  cover: "https://img.ananweb.jp/2019/10/12123242/311-455x600.jpg",
+};
+const song31 = {
+  name: "どろん",
+  path: "music/King Gnu - どろん.mp3",
+  artist: "King Gnu",
+  cover: "https://images-fe.ssl-images-amazon.com/images/I/41WKgaVegFL.jpg",
+};
+const song32 = {
+  name: "心眼",
+  path: "music/Lanndo feat.須田景凪「心眼」MV.mp3",
+  artist: "Lanndo",
+  cover:
+    "https://www.toysfactory.co.jp/artist_images/008765_0kD2AfKgnRjDHBDT78qDcUGOo0OqDA.jpg",
+};
+const song33 = {
+  name: "LEO",
+  path: "music/LEO - Eve MV.mp3",
+  artist: "Eve",
+  cover:
+    "https://ogre.natalie.mu/artist/104938/20201223/EVE_art202012.jpg?imwidth=640&imdensity=1",
+};
+const song34 = {
+  name: "crossing field",
+  path: "music/LiSA「crossing field」／「ソードアート・オンライン」第1期1stクール ノンクレジットOP.mp3",
+  artist: "LiSA",
+  cover: "https://m.media-amazon.com/images/I/91JMI+UoYcL._AC_SL1500_.jpg",
+};
+const song35 = {
+  name: "WanteD!",
+  path: "music/Mrs. GREEN APPLE - WanteD! WanteD!.mp3",
+  artist: "Mrs. GREEN APPLE",
+  cover: "https://m.media-amazon.com/images/I/61EqXcCkdtL._AC_SX355_.jpg",
+};
+const song36 = {
+  name: "予感",
+  path: "music/SUPER BEAVER「予感」MV.mp3",
+  artist: "Super Beaver",
+  cover:
+    "https://www.billboard-japan.com/scale/news/00000087/87407/800x_image.jpg",
+};
+const song37 = {
+  name: "たぶん",
+  path: "music/YOASOBI「たぶん」Official Music  Video (1).mp3",
+  artist: "YOASOBI",
+  cover:
+    "https://is2-ssl.mzstatic.com/image/thumb/Music115/v4/4b/ba/5b/4bba5b81-c56e-1513-4326-e24daf4a7bff/195497666737.jpg/600x600bf-60.jpg",
+};
+const song38 = {
+  name: "劣等上等",
+  path: "music/劣等上等 歌ってみたのはメガテラ・ゼロ (1).mp3",
+  artist: "メガテラ・ゼロ",
+  cover:
+    "https://yt3.ggpht.com/ytc/AMLnZu81hRrvsJW5OeCpF9SzjK25lir7iEQTXQ_ofgM8=s900-c-k-c0x00ffffff-no-rj",
+};
+
+const song39 = {
+  name: "暗く黒く",
+  path: "music/ずっと真夜中でいいのに。『暗く黒く』MV（ZUTOMAYO - DARKEN）.mp3",
+  artist: "ずっと真夜中でいいのに",
+  cover:
+    "https://is2-ssl.mzstatic.com/image/thumb/Music115/v4/4b/ba/5b/4bba5b81-c56e-1513-4326-e24daf4a7bff/195497666737.jpg/600x600bf-60.jpg",
+};
+const song40 = {
+  name: "きみも悪い人でよ",
+  path: "music/そらる-きみも悪い人でよかった【歌ってみた】.mp3",
+  artist: "そらる",
+  cover:
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnDr5unbf4BHbn33LKkA0AAEUdXSj1Qhs43rnv9Hjxm9TK-tFkSfeUvW2IB-vI0JxHPi8&usqp=CAU",
+};
+const song41 = {
+  name: "Apollo",
+  path: "music/ポルノグラフィティ『アポロ』(_OPEN MUSIC CABINET_LIVE IN SAITAMA SUPER ARENA 2007_) _ PORNOGRAFFITTI『Apollo(Live)』.mp3",
+  artist: "ポルノグラフィティ",
+  cover: "https://news.mynavi.jp/article/20180914-693233/images/001.jpg",
+};
+const song42 = {
+  name: "ヲズワルド",
+  path: "music/ヲズワルド - 煮ル果実 （Cover） _ いゔどっと.mp3",
+  artist: "いゔどっと",
+  cover: "https://pbs.twimg.com/media/E104RBCUYAA3X3A.jpg",
+};
+const song43 = {
+  name: "ドライフラワー",
+  path: "music/優里 『ドライフラワー』 Official Music Video -ディレクターズカットver.-.mp3",
+  artist: "優里",
+  cover:
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6tDy8qu7mO9w7-pe7JZr4HjshDtPGseU4BhQ43htRC62R37ua7wsbkdIvzZ3xVLI-p6E&usqp=CAU",
+};
+const song44 = {
+  name: "アイボリー",
+  path: "music/アイボリー 歌ってみたのはメガテラ・ゼロ.mp3",
+  artist: "メガテラ・ゼロ",
+  cover:
+    "https://yt3.ggpht.com/ytc/AMLnZu81hRrvsJW5OeCpF9SzjK25lir7iEQTXQ_ofgM8=s900-c-k-c0x00ffffff-no-rj",
+};
+const song45 = {
+  name: "君の神様になりたい",
+  path: "music/kimi.mp3",
+  artist: "メガテラ・ゼロ",
+  cover:
+    "https://yt3.ggpht.com/ytc/AMLnZu81hRrvsJW5OeCpF9SzjK25lir7iEQTXQ_ofgM8=s900-c-k-c0x00ffffff-no-rj",
+};
+const song46 = {
+  name: "灰色と青",
+  path: "music/米津玄師  - 灰色と青（ ＋菅田将暉 ）,  Kenshi Yonezu - Haiirotoao（+Masaki Suda).mp3",
+  artist: "Kenshi Yonezu",
+  cover:
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWR9ZvdV5qd2EroI9P-K7la8QhX6IYvSMhwg&usqp=CAU",
+};
+const song47 = {
+  name: "main actor",
+  path: "music/美波「main actor 」MV.mp3",
+  artist: "美波",
+  cover:
+    "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx120607-XujIeO90fbi7.png",
+};
+const song48 = {
+  name: "阿吽のビーツ",
+  path: "music/阿吽のビーツ　歌ってみたのはメガテラ・ゼロ.mp3",
+  artist: "メガテラ・ゼロ",
+  cover:
+    "https://yt3.ggpht.com/ytc/AMLnZu81hRrvsJW5OeCpF9SzjK25lir7iEQTXQ_ofgM8=s900-c-k-c0x00ffffff-no-rj",
+};
+const song49 = {
+  name: "雨とペトラ",
+  path: "music/雨とペトラ _ バルーン(Cover) めいちゃん.mp3",
+  artist: "めいちゃん",
+  cover:
+    "https://yt3.ggpht.com/ytc/AKedOLT7246eM4g2AwoED1cZGlyzy60auMoWkhZPm2WDyg=s900-c-k-c0x00ffffff-no-rj",
+};
+let songs = [
+  song1,
+  song2,
+  song3,
+  song4,
+  song5,
+  song6,
+  song7,
+  song8,
+  song9,
+  song10,
+  song11,
+  song12,
+  song13,
+  song14,
+  song15,
+  song16,
+  song17,
+  song18,
+  song19,
+  song20,
+  song21,
+  song22,
+  song23,
+  song24,
+  song25,
+  song26,
+  song27,
+  song28,
+  song29,
+  song30,
+  song31,
+  song32,
+  song33,
+  song34,
+  song35,
+  song36,
+  song37,
+  song38,
+  song39,
+  song40,
+  song41,
+  song42,
+  song43,
+  song44,
+  song45,
+  song46,
+  song47,
+  song48,
+  song49,
+];
